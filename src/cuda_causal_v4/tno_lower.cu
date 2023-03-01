763ac9fb39a00a2450eee82dfe22805c47e6717c
Doraemonzzz <715073608@qq.com>
2023-03-02 00:09:47 +0800

    update v4

--- src/cuda_causal_v4/tno_lower.cu
+++ src/cuda_causal_v4/tno_lower.cu
@@ -1,6 +1,7 @@
 #include<stdio.h>
-#define e 2
-#define c 16
+#define b_ 2
+#define n_ 16
+#define d_ 64
 
 template<typename F>
 __global__ void lower_kernel(const int b, const int d, const int n, const F* T, const F* x, F* y) {
@@ -73,13 +74,13 @@ __global__ void backward_kernel(const int b, const int d, const int n, const F* 
 }
 
 void forward_cuda(int b, int d, int n, float* T, float* x, float* y) {
-    dim3 DimGrid((b + e - 1) / e, (n + c - 1) / c, (d + c - 1) / c);
-    dim3 DimBlock(e, c, c);
+    dim3 DimGrid((b + b_ - 1) / b_, (n + n_ - 1) / n_, (d + d_ - 1) / d_);
+    dim3 DimBlock(b_, n_, d_);
     lower_kernel<<<DimGrid, DimBlock>>>(b, d, n, T, x, y);
 }
 
 void backward_cuda(int b, int d, int n, float* T, float* x, float* gy, float* gT, float* gx) {
-    dim3 DimGrid((b + e - 1) / e, (n + c - 1) / c, (d + c - 1) / c);
-    dim3 DimBlock(e, c, c);
+    dim3 DimGrid((b + b_ - 1) / b_, (n + n_ - 1) / n_, (d + d_ - 1) / d_);
+    dim3 DimBlock(b_, n_, d_);
     backward_kernel<<<DimGrid, DimBlock>>>(b, d, n, T, x, gy, gT, gx);
 }
